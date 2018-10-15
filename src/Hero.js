bad3b30a137f2cf3e26bbcefc05ad9139485883b
Nikita Mabatov <nikita-mabatov@mail.ru>
2018-10-15 16:53:18 +0300

    first commit

---
+++ src/Hero.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './Hero.css'
+
+class Hero extends Component {
+	render() {
+		return (
+			<div className = "f3 tc">
+				<h1>Hello!</h1>
+				<p>Welcome to React</p>
+			</div>
+			);
+	}
+}
+
+export default Hero;
